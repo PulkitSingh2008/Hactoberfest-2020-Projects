7cc1b56abff38748a589ed132214dc7e8c9c305b
PulkitSingh2008 <72497714+PulkitSingh2008@users.noreply.github.com>
2020-10-10 16:14:29 +0530

    Update Logical program.py

--- Logical program.py
+++ Logical program.py
@@ -1,1 +1,15 @@
+print("Calculator")
+num1=float(input())
+operator=input()
+num2=float(input())
 
+if operator == "+":
+  print(num1+num2)
+elif operator == "*":
+  print(num1*num2)
+elif operator == "/":
+  print(num1/num2)
+elif operator == "-":
+  print(num1-num2)
+else:
+  print("Operator not found")
